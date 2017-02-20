849ab31dc2bf46426955c8e6d56cf3bedfcee84c
Cristiano <cristianornelas@hotmail.com>
2017-02-20 09:19:50 -0800

    topologia 2

--- projeto2/lab3/mn.py
+++ projeto2/lab3/mn.py
@@ -11,6 +11,8 @@ from mininet.node import Switch, OVSKernelSwitch, Node
  
 def main():
 	os.system("mn -c >/dev/null 2>&1")
+	os.system("killall -9 zebra bgpd > /dev/null 2>&1")
+
 
 	net = Mininet()
 	c0 = net.addController()
@@ -20,6 +22,7 @@ def main():
 	net.addLink(h1, s1)
 	net.addLink(h2, s1)
 
+	#c1 = net.addController()
 	s2 = net.addSwitch("s2")
 	h3 = net.addHost("h3")
 	h4 = net.addHost("h4")
@@ -33,11 +36,25 @@ def main():
 	s2.cmd("sysctl -w net.ipv4.ip_forward=1")
 	print "Wait 3 seconds"
 	sleep(3)
+        '''print "Configuring router"
+	s1.cmd("/usr/lib/quagga/zebra -f conf/s1.conf -d -i /tmp/zebraS1.pid > S1-zebra-stdout 2>&1")'''
 
 	net.start()
 
+	'''h1.cmd("ifconfig h1-eth0 10.0.1.2/24")
+	h1.waitOutput()
+	h1.cmd("route add default gw 10.0.1.1")
+	h1.waitOutput()
+
+	h2.cmd("ifconfig h2-eth0 10.0.3.2/24")
+	h2.waitOutput()
+	h2.cmd("route add default gw 10.0.3.1")
+	h2.waitOutput()'''
+
+	#net.start()
 	CLI(net)
 	net.stop()
+	os.system("killall -9 zebra bgpd")
 
 if __name__ == "__main__":
 	main()
